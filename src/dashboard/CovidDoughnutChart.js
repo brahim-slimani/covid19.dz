e5c58304147532ec6581eb264991a403be2d45d5
Slimani-Ibrahim <slimani.ibr@gmail.com>
2020-07-06 22:21:59 +0100

    add recovered cases in today report

--- src/dashboard/CovidDoughnutChart.js
+++ src/dashboard/CovidDoughnutChart.js
@@ -29,7 +29,7 @@ class CovidDoughnutChart extends React.Component{
                 axes = ['active cases','deaths','recovered'];
                 break;
             case 'today':
-                axes = ['new cases','deaths'];
+                axes = ['new cases','deaths', 'recovered'];
                 break;
         }
         return axes;
@@ -42,7 +42,7 @@ class CovidDoughnutChart extends React.Component{
                 axes = [data.active, data.deaths, data.recovered];
                 break;
             case 'today':
-                axes = [data.todayCases, data.todayDeaths];
+                axes = [data.todayCases, data.todayDeaths, data.todayRecovered];
                 break;
         }
         return axes;
